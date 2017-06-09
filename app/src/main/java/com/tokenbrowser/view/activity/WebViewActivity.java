459a038681c3fe718aea85202aaa9d532eb7e561
Steven Ireland <scientastersteve@gmail.com>
2017-06-08 20:55:56 -0400

    make audio go

--- app/src/main/java/com/tokenbrowser/view/activity/WebViewActivity.java
+++ app/src/main/java/com/tokenbrowser/view/activity/WebViewActivity.java
@@ -17,9 +17,19 @@
 
 package com.tokenbrowser.view.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 import com.tokenbrowser.R;
 import com.tokenbrowser.databinding.ActivityWebViewBinding;
@@ -32,6 +42,7 @@ public class WebViewActivity extends BasePresenterActivity<WebViewPresenter, Web
 
     public static final String EXTRA__ADDRESS = "address";
     private ActivityWebViewBinding binding;
+    private PermissionRequest myRequest;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -41,15 +52,78 @@ public class WebViewActivity extends BasePresenterActivity<WebViewPresenter, Web
 
     private void init() {
         this.binding = DataBindingUtil.setContentView(this, R.layout.activity_web_view);
+        Activity thisActivity = this;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
         WebView webview = (WebView) findViewById(R.id.webview);
-        webview.setWebChromeClient(new WebChromeClient(){
+        webview.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
-                request.grant(request.getResources());
+                myRequest = request;
+                if (ContextCompat.checkSelfPermission(thisActivity,
+                        Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(thisActivity,
+                            new String[]{Manifest.permission.RECORD_AUDIO},
+                            4);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                } else {
+                    myRequest.grant(myRequest.getResources());
+                }
             }
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 4: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0   && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    myRequest.grant(myRequest.getResources());
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    public void askForPermission(String origin, String permission, int requestCode) {
+        Log.d("WebView", "inside askForPermission for" + origin + "with" + permission);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    permission)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+
+            }
+        } else {
+        }
+    }
     public final ActivityWebViewBinding getBinding() {
         return this.binding;
     }
